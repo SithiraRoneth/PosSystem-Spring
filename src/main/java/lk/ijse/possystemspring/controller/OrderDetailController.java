278e7a9a567698c799cc4fafd07f3e568a17a73b
USER <sithiraroneth@gmail.com>
2024-10-11 00:44:36 +0530

    Edit OrderDetailController

--- src/main/java/lk/ijse/possystemspring/controller/OrderDetailController.java
+++ src/main/java/lk/ijse/possystemspring/controller/OrderDetailController.java
@@ -5,11 +5,17 @@
  * */
 package lk.ijse.possystemspring.controller;
 
+import lk.ijse.possystemspring.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/orderDetails")
 public class OrderDetailController {
-
+    @Autowired
+    private OrderService orderService;
+    static Logger logger = LoggerFactory.getLogger(OrderDetailController.class);
 }
